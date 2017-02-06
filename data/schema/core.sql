9b82c90b376412ecdffece9b2767c178e0d92d55
Vadim L. Avramishin <vavramishin@brightgrove.com>
2017-02-07 01:20:01 +0200

    db schema for core functionality

--- data/schema/core.sql
+++ data/schema/core.sql
@@ -1,22 +1,12 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Версия сервера:               5.7.14 - MySQL Community Server (GPL)
--- Операционная система:         Win64
--- HeidiSQL Версия:              9.4.0.5148
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-
--- Дамп структуры базы данных air
 CREATE DATABASE IF NOT EXISTS `air` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `air`;
 
--- Дамп структуры для таблица air.acl_objects
 CREATE TABLE IF NOT EXISTS `acl_objects` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
@@ -25,8 +15,6 @@ CREATE TABLE IF NOT EXISTS `acl_objects` (
   KEY `name` (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
--- Дамп структуры для таблица air.acl_roles
 CREATE TABLE IF NOT EXISTS `acl_roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
@@ -35,8 +23,6 @@ CREATE TABLE IF NOT EXISTS `acl_roles` (
   KEY `name` (`name`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
--- Дамп структуры для таблица air.acl_roles_objects
 CREATE TABLE IF NOT EXISTS `acl_roles_objects` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `role_id` int(11) unsigned NOT NULL COMMENT 'getRole(acl_roles.id)',
@@ -47,8 +33,6 @@ CREATE TABLE IF NOT EXISTS `acl_roles_objects` (
   KEY `object_id` (`object_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
--- Дамп структуры для таблица air.acl_users_roles
 CREATE TABLE IF NOT EXISTS `acl_users_roles` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL COMMENT 'getUser(users.id)',
@@ -58,8 +42,6 @@ CREATE TABLE IF NOT EXISTS `acl_users_roles` (
   KEY `role_id` (`role_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
--- Дамп структуры для таблица air.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) DEFAULT NULL,
@@ -70,7 +52,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Экспортируемые данные не выделены.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
